0f090b871d84b73a8ae6314a1f7226acd7dfccad
Gayan Sachintha <118958174+Gayan-Sachintha@users.noreply.github.com>
2023-12-29 00:00:29 +0530

    Update buyfresh.sql

--- buyfresh.sql
+++ buyfresh.sql
@@ -58,12 +58,6 @@ CREATE TABLE IF NOT EXISTS `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `Email`, `Name`, `Price`, `Path`, `Post`) VALUES
-(49, 'pipins365@gmail.com', 'Cabbage', 'Rs : 120', 'uploads/index1.jpg', 0),
-(48, 'pipins365@gmail.com', 'Leek', 'Rs : 60', 'uploads/index.jpg', 1),
-(47, 'pipins365@gmail.com', 'Carrot', 'Rs : 80', 'uploads/carrot-icon-cartoon-style-on-white-background-vector-8177752.jpg', 1),
-(46, 'pipins365@gmail.com', 'Broccoli', 'Rs : 30', 'uploads/broccoli-vegetable-cartoon-vector-1268538.jpg', 1),
-(45, 'pipins365@gmail.com', 'Beetroot', 'Rs : 50', 'uploads/beetroot-cartoon-style-vegetarian-fresh-food-vector-14097247.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -86,12 +80,7 @@ CREATE TABLE IF NOT EXISTS `productb` (
 -- Dumping data for table `productb`
 --
 
-INSERT INTO `productb` (`id`, `Email`, `Name`, `Price`, `Path`, `Post`) VALUES
-(3, 'pipins365@gmail.com', 'Cake', 'Rs : 1800', 'uploads/delicious-cake-cartoon-vector-illustration-served-sweet-pastry-creamy-bakery-flat-color-object-confection-baked-sugary-dessert-188028083.jpg', 0),
-(4, 'pipins365@gmail.com', 'Muffin', 'Rs : 120', 'uploads/isolated-blueberry-muffin-white-background-drawing-illustration-133615606.jpg', 0),
-(5, 'pipins365@gmail.com', 'Cookie', 'Rs : 125', 'uploads/sweet-cookie-cartoon-vector-24442879.jpg', 0),
-(6, 'pipins365@gmail.com', 'Rolls', 'Rs : 120', 'uploads/360_F_427659028_5dsX7Jt7uxew1SeBpfdzA5bVGvtctZYp.jpg', 0),
-(7, 'pipins365@gmail.com', 'Bread', 'Rs : 150', 'uploads/cartoon-illustration-isolated-object-delicious-flour-food-bakery-bread-whole-grain-toast-vector.jpg', 0);
+
 
 -- --------------------------------------------------------
 
@@ -114,12 +103,7 @@ CREATE TABLE IF NOT EXISTS `productl` (
 -- Dumping data for table `productl`
 --
 
-INSERT INTO `productl` (`id`, `Email`, `Name`, `Price`, `Path`, `Post`) VALUES
-(6, 'pipins365@gmail.com', 'Bacardi', 'Rs : 2400', 'uploads/bacardi-razz-1l.jpg', 1),
-(5, 'pipins365@gmail.com', 'Arrack', 'Rs : 1800', 'uploads/scsl-exta-special.png', 1),
-(7, 'pipins365@gmail.com', 'Campari', 'Rs : 3000', 'uploads/images.jpg', 0),
-(8, 'pipins365@gmail.com', 'Frenet Brance', 'Rs : 2000', 'uploads/08038cf00114189c3b5e923017b377d3.jpg', 0),
-(9, 'pipins365@gmail.com', 'TripaleSec', 'Rs : 2900', 'uploads/pol_pl_Triple-Sec-Coloniale-Liker-Barmanski-40-1-0l-7732_1.jpg', 0);
+
 
 -- --------------------------------------------------------
 
@@ -142,13 +126,6 @@ CREATE TABLE IF NOT EXISTS `productm` (
 -- Dumping data for table `productm`
 --
 
-INSERT INTO `productm` (`id`, `Email`, `Name`, `Price`, `Path`, `Post`) VALUES
-(3, 'pipins365@gmail.com', 'Beef', 'Rs : 1800', 'uploads/bbq-steak-icon-flat-style-vector-20612902.jpg', 0),
-(4, 'pipins365@gmail.com', 'Mutton', 'Rs : 1500', 'uploads/cartoon-leg-lamb-vector-38434210.jpg', 0),
-(5, 'pipins365@gmail.com', 'Crab', 'Rs : 1600', 'uploads/cartoon-red-crab-vector-4043286.jpg', 0),
-(6, 'pipins365@gmail.com', 'Pork', 'Rs : 1000', 'uploads/pork-meat-skin-icon-cartoon-style-vector-31776504.jpg', 0),
-(7, 'pipins365@gmail.com', 'Chicken', 'Rs : 1200', 'uploads/whole-roast-chicken-isolated-vector-15345611.jpg', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -169,10 +146,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`Fullname`, `Email`, `Cnumber`, `Password`, `utype`) VALUES
-('Naveen Senanayake', 'pipins365@gmail.com', 778455138, '1234', 'admin'),
-('java', 'java@gmail.com', 11, '555', 'customer');
-COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
